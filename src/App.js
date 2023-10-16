024e8f66b07f18e786aa6dd8f31fe4df5e8a272d
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-10-16 12:24:50 +0530

    sahil

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-
-
-
-
 import { useEffect, useState } from 'react';
 import { ethers } from 'ethers';
 
@@ -19,12 +15,64 @@ import config from './config.json';
 
 function App() {
 
+  const [provider, setProvider] = useState(null)
+
+  const [account, setAccount] = useState(null)
+
+  const loadBlockchainData = async () => {
+    
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+
+    const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider)
+    const totalSupply = await realEstate.totalSupply()
+    console.log(totalSupply.toString())
+
+      config[network.chainId].escrow.address
+
+
+      window.ethereum.on('accountChanged', async () => {
+        const accounts = await window.ethereum.request({method: 'eth_requestAccounts' });
+        const account = ethers.utils.getAddress(accounts[0])
+        setAccount(account);
+      })
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
   return (
     <div>
 
+      <Navigation account={account} setAccount={setAccount} />
+
+      <Search/>
+
       <div className='cards__section'>
 
-        <h3>Welcome to Your Dream Destination</h3>
+        <h3>Homes For You</h3>
+
+        <hr/>
+
+        <div className='cards'>
+          <div className='card'>
+            <div className='card__image'>
+              <img src='' alt='Home' />
+            </div>
+            <div className='card__info'>
+              <h4> 1 ETH</h4>
+              <p>
+                <strong>1</strong> beds |
+                <strong>2</strong> bath |
+                <strong>1000</strong> sqft 
+              </p>
+              <p>1234 omaxe city</p>
+            </div>
+          </div>
+        </div>
 
       </div>
 
