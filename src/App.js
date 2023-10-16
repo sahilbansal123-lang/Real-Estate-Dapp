b4e45025c312c09b89ec93bf8ea2702492c5a9ef
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-10-16 12:42:35 +0530

    sa

--- src/App.js
+++ src/App.js
@@ -1,81 +1,94 @@
-import { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
+import { useEffect, useState } from "react";
+import { ethers } from "ethers";
 
 // Components
-import Navigation from './components/Navigation';
-import Search from './components/Search';
-import Home from './components/Home';
+import Navigation from "./components/Navigation";
+import Search from "./components/Search";
+import Home from "./components/Home";
 
 // ABIs
-import RealEstate from './abis/RealEstate.json'
-import Escrow from './abis/Escrow.json'
+import RealEstate from "./abis/RealEstate.json";
+import Escrow from "./abis/Escrow.json";
 
 // Config
-import config from './config.json';
+import config from "./config.json";
 
 function App() {
-
-  const [provider, setProvider] = useState(null)
-
-  const [account, setAccount] = useState(null)
+  const [provider, setProvider] = useState(null);
+  const [escrow, setEscrow] = useState(null);
+  const [account, setAccount] = useState(null);
+  const [homes, setHomes] = useState([]);
 
   const loadBlockchainData = async () => {
-    
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider)
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
 
-    const network = await provider.getNetwork()
+    const network = await provider.getNetwork();
 
-    const realEstate = new ethers.Contract(config[network.chainId].realEstate.address, RealEstate, provider)
-    const totalSupply = await realEstate.totalSupply()
-    console.log(totalSupply.toString())
+    const realEstate = new ethers.Contract(
+      config[network.chainId].realEstate.address,
+      RealEstate,
+      provider
+    );
+    const totalSupply = await realEstate.totalSupply();
+    const homes = [];
 
-      config[network.chainId].escrow.address
+    for (var i = 1; i < totalSupply; i++) {
+      const uri = await realEstate.tokenURI(i);
+      const response = await fetch(uri);
+      const metadata = await response.json();
+      homes.push(metadata);
+    }
+    setHomes(homes);
+    console.log(homes);
 
+    const escrow = new ethers.Contract(
+      config[network.chainId].escrow.address,
+      Escrow,
+      provider
+    );
+    setEscrow(escrow);
 
-      window.ethereum.on('accountChanged', async () => {
-        const accounts = await window.ethereum.request({method: 'eth_requestAccounts' });
-        const account = ethers.utils.getAddress(accounts[0])
-        setAccount(account);
-      })
-  }
+    window.ethereum.on("accountChanged", async () => {
+      const accounts = await window.ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      const account = ethers.utils.getAddress(accounts[0]);
+      setAccount(account);
+    });
+  };
 
   useEffect(() => {
-    loadBlockchainData()
-  }, [])
+    loadBlockchainData();
+  }, []);
 
   return (
     <div>
-
       <Navigation account={account} setAccount={setAccount} />
 
-      <Search/>
+      <Search />
 
-      <div className='cards__section'>
-
+      <div className="cards__section">
         <h3>Homes For You</h3>
 
-        <hr/>
+        <hr />
 
-        <div className='cards'>
-          <div className='card'>
-            <div className='card__image'>
-              <img src='' alt='Home' />
+        <div className="cards">
+          <div className="card">
+            <div className="card__image">
+              <img src="" alt="Home" />
             </div>
-            <div className='card__info'>
+            <div className="card__info">
               <h4> 1 ETH</h4>
               <p>
-                <strong>1</strong> beds |
-                <strong>2</strong> bath |
-                <strong>1000</strong> sqft 
+                <strong>1</strong> beds |<strong>2</strong> bath |
+                <strong>1000</strong> sqft
               </p>
               <p>1234 omaxe city</p>
             </div>
           </div>
         </div>
-
       </div>
-
     </div>
   );
 }
